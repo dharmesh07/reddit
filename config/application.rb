59ffdda3623674acfc169e063e7496dfc44ba547
dharmesh07 <rrdharmesh@gmail.com>
2017-01-23 23:57:52 +0530

    Added google-Oauth2 for signup integration

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,7 @@ module Reddit
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  if Rails.env.development? 
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
+end
 end
