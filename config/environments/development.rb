5fe60648120ef26a2d070691cb4bdb877e302e57
dharmesh07 <rrdharmesh@gmail.com>
2017-01-25 00:06:22 +0530

    Revert "Revert "Sending welcome mail using mailer for users signing up using gmail smtp""
    This reverts commit 441a59ba49126dd66b247b43e92855b3e2078a0f.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # ActionMailer Config
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: 'rrdharmesh@gmail.com',
+  password: 'ampositive'
+}
+# change to true to allow email to be sent during development
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
